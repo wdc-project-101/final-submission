e3e4d496a15428af1227b6e6a26d39cf44b24354
WDC Project 101 <wdcproject101@gmail.com>
2023-10-02 08:06:35 +0000

    Mon, Oct 2, 2023, 6:36 PM +10:30

--- app.js
+++ app.js
@@ -1,50 +1,20 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const bodyParser = require('body-parser');
-const logger = require("morgan");
-const session = require("express-session"); // sessions
-const mysql = require("mysql");
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const hostsRouter = require("./routes/hosts");
-const adminRouter = require("./routes/admin");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// create pool
-var dbConnectionPool = mysql.createPool(
-{
-  host: "localhost",
-  database: "project"
-});
+var app = express();
 
-const app = express();
-app.use(bodyParser.json());
-
-app.use(function(req, res, next)
-{
-  req.pool = dbConnectionPool;
-  next();
-});
-
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// create session
-app.use(session(
-{
-  secret: "encryption key",
-  resave: false,
-  saveUninitialized: true,
-  cookie: {secure: false}
-}));
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/users/hosts", hostsRouter);
-app.use("/users/admin", adminRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
